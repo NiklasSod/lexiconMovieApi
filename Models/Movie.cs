df5903762494d6a63d45e7549d3ea5ee6f752959
NiklasSod <nma.prob@gmail.com>
2026-06-15 15:06:39 +0200

    add Movie model

---
+++ Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public int Duration { get; set; }
+
+        //public int GenreId { get; set; }
+        //public Genre? Genre { get; set; }
+
+        //public MovieDetails? Details { get; set; }
+
+        //public List<Review> Reviews { get; set; } = new();
+
+        //public List<MovieActor> MovieActors { get; set; } = new();
+    }
+}
