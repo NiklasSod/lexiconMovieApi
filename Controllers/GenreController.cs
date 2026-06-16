5019177a32680d9e7d4835f206a8791ea152fc83
NiklasSod <nma.prob@gmail.com>
2026-06-17 00:49:12 +0200

    add DTO, Post and Get for Genre

---
+++ Controllers/GenreController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.DTOs.Genre;
+using MovieApi.Models;
+
+namespace MovieApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly MovieApiContext _context;
+        public GenreController(MovieApiContext context)
+        {
+            _context = context;
+        }
+
+        // Post: api/Genre
+        [HttpPost]
+        public async Task<ActionResult<Genre>> CreateGenre([FromBody] GenreCreateDto genreDto)
+        {
+            var exists = await _context.Genres
+                .AnyAsync(g => g.Name.ToLower() == genreDto.Name.ToLower());
+
+            if (exists)
+            {
+                return BadRequest("Genre already exists.");
+            }
+
+            var genre = new Genre {
+                Name = genreDto.Name
+            };
+
+            _context.Genres.Add(genre);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetGenreById), new { id = genre.Id }, genre);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Genre>> GetGenreById(int id)
+        {
+            var genre = await _context.Genres.FindAsync(id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return genre;
+        }
+    }
+}
---
+++ DTOs/Genre/GenreCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.DTOs.Genre
+{
+    public class GenreCreateDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
